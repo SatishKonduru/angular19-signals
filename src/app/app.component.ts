aa09d853f0f8a168544cf0c78dd3f53315229c2c
satish <satishkonduru0225@gmail.com>
2024-12-02 11:17:55 +0530

    async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'angular19-signals';
 
-  counter = 0;
-  // readonly changeDetector = inject(ChangeDetectorRef)
-  constructor(){
-    setInterval(() => {
-      this.counter++
-      console.log("Counter Value: ", this.counter)
-    }, 1000)
+  counter$ = interval(1000)
 
-    // setInterval(() => {
-    //   this.changeDetector.detectChanges()
-    // }, 5000)
-  }
+  constructor(){}
 
   randomNumber(){
-     console.log("Random Number: ",Math.random())
-
+    console.log("Random Number called...")
+    return 100;
   }
 
-  doNothing(){
-    console.log("Nothing......")
-  }
 }
