8978e12e55dac2e5bad6936e02cc6c691445450e
satish <satishkonduru0225@gmail.com>
2024-12-02 10:16:58 +0530

    Default Behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppComponent {
   title = 'angular19-signals';
+
   counter = 0;
-  readonly changeDetector = inject(ChangeDetectorRef)
+  // readonly changeDetector = inject(ChangeDetectorRef)
   constructor(){
     setInterval(() => {
       this.counter++
       console.log("Counter Value: ", this.counter)
     }, 1000)
 
-    setInterval(() => {
-      this.changeDetector.detectChanges()
-    }, 5000)
+    // setInterval(() => {
+    //   this.changeDetector.detectChanges()
+    // }, 5000)
   }
 
-  doNothing(){}
+  randomNumber(){
+     console.log("Random Number: ",Math.random())
+
+  }
+
+  doNothing(){
+    console.log("Nothing......")
+  }
 }
