6b459dbe9727e026743b8f4d7157550ce84acad0
satish <satishkonduru0225@gmail.com>
2024-12-06 09:54:34 +0530

    effect

--- src/app/components/counter-with-signals/counter-with-signals.component.ts
+++ src/app/components/counter-with-signals/counter-with-signals.component.ts
@@ -1,18 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, EffectRef, inject, Injector, runInInjectionContext, signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-counter-with-signals',
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './counter-with-signals.component.html',
   styleUrl: './counter-with-signals.component.css'
 })
 export class CounterWithSignalsComponent {
   readonly value = signal(0)
+  readonly injector = inject(Injector)
+  private _effectRef: EffectRef | null = null
   constructor(){
    setInterval(() => {
       this.value.update(v => v+1)
-      console.log(" Value: ", this.value())
     }, 1000)
+
+
+  }
+
+  start(){
+    // runInInjectionContext(this.injector, () => {
+    //   effect(() => console.log("Value: ", this.value()))
+    // })
+    this._effectRef = effect(() => {console.log("Value: ", this.value())} , {injector: this.injector})
+  }
+  stop(){
+    this._effectRef.destroy()
+    this._effectRef = null
   }
 }
