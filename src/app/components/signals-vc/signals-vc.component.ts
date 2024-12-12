f30735997aa1b4c5c58f7376da2fbd080d3a7942
satish <satishkonduru0225@gmail.com>
2024-12-12 12:57:29 +0530

    signals working

--- src/app/components/signals-vc/signals-vc.component.ts
+++ src/app/components/signals-vc/signals-vc.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, signal, Signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, signal, Signal, ViewChild, effect, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-signals-vc',
@@ -7,24 +7,45 @@ import { Component, ElementRef, signal, Signal, ViewChild } from '@angular/core'
   styleUrl: './signals-vc.component.css'
 })
 export class SignalsVcComponent {
-  @ViewChild('header') headerRef : ElementRef;
+  // @ViewChild('header',{read: signal}) headerRef : ElementRef;
+  // @ViewChildren('item', {read:signal}) itemsRef!: QueryList<ElementRef>;
   colorSignal = signal('red');
-
+  itemsList = ['Item-1', 'Item-2', 'Item-3', 'Item-4'];
   constructor(){
-    this.startColorChange();
+    // this.startColorChange();
+    // this.startColorToggle();
+    // effect(() => {
+    //   this.startColorChange();
+    //   this.startColorToggle();
+    // })
   }
+   // effect declared directly in the class body
+   private colorEffect = effect(() => {
+    this.startColorChange();
+    this.startColorToggle();
+  });
 
+
+  colors  = this.itemsList.map(() => signal(''));
   startColorChange() {
     setInterval(() => {
-
       const randomColor = this.getRandomColor();
       this.colorSignal.set(randomColor);
+
     }, 1000);
   }
+  private startColorToggle(): void {
 
+    setInterval(() => {
+
+        this.itemsList.forEach((_, index) => {
+        const newColor = this.getRandomColor();
+        this.colors[index].set(newColor); // Update the color signal
+      });
+    }, 2000); // Update colors every 2 seconds
+  }
   private getRandomColor(): string {
     console.log("Random color: ", `#${Math.floor(Math.random() * 16777215).toString(16)}`)
-
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 }
